881f096f57bad8e5590f409888a9e222345af689
icyethics <97385843+icyethics@users.noreply.github.com>
2026-04-17 13:47:56 +0200

    Fixed Novalite
    For some reason it was set up to just add 1 to all player-facing numbers in the UI. Now it works as described

--- content/violentviolets/novalite.lua
+++ content/violentviolets/novalite.lua
@@ -12,10 +12,10 @@ SMODS.Joker { -- :3
     soul_pos = {x = 5, y = 1},
     blueprint_compat = true,
     loc_vars = function(self, info_queue, card)
-        local planets_used = 0
+        local planets_used = 1
         for k, v in pairs(G.GAME.consumeable_usage) do if v.set == 'Planet' then planets_used = planets_used + 1 end end
         return { 
-            vars = { card.ability.extra.multi, (planets_used + 1) }, 
+            vars = { card.ability.extra.multi, (planets_used) }, 
         }
     end,
     ppu_team = { "VV" },
@@ -25,14 +25,14 @@ SMODS.Joker { -- :3
         if context.setting_blind then
             G.E_MANAGER:add_event(Event({
                 func = function()
-                    local planets_used = 0
+                    local planets_used = 1
                     for k, v in pairs(G.GAME.consumeable_usage) do
                         if v.set == 'Planet' then planets_used = planets_used + 1 end
                     end
                     ease_discard(planets_used)
                     ease_hands_played(planets_used)
                     SMODS.calculate_effect(
-                        { message = '+' .. tostring(planets_used + 1) .. ' Hands & Discards' },
+                        { message = '+' .. tostring(planets_used) .. ' Hands & Discards' },
                         context.blueprint_card or card)
                     return true
                 end
