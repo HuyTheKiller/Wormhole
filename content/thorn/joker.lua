d877e34ac11140ae0cb3cb4ff7475f6dd0cf28cd
the-Astra <djs8743@rit.edu>
2026-04-22 19:27:38 -0400

    Fix #85

--- content/thorn/joker.lua
+++ content/thorn/joker.lua
@@ -6,57 +6,57 @@ SMODS.Atlas {
 }
 
 SMODS.Joker {
-	key = 'thorn_jimbo',
-	rarity = 1,
+    key = 'thorn_jimbo',
+    rarity = 1,
     atlas = "thorn_cards",
-	pos = { x = 0, y = 0 },
-    soul_pos = { x = 2, y = 1}, --I don't want to rearange the atlas :з
-	cost = 5,
-	blueprint_compat = true,
-	eternal_compat = false,
-	perishable_compat = true,
+    pos = { x = 0, y = 0 },
+    soul_pos = { x = 2, y = 1 }, --I don't want to rearange the atlas :з
+    cost = 5,
+    blueprint_compat = true,
+    eternal_compat = false,
+    perishable_compat = true,
     jand_gemini_compat = false, -- Me making our Jokers compatible with my shit... Hehehehe :Þ
-    attributes = {'space', 'planet', "generation", "hands"},
+    attributes = { 'space', 'planet', "generation", "hands" },
     loc_vars = function(self, info_queue, card)
-        return { vars = {G.PROFILES[G.SETTINGS.profile].name}}
+        return { vars = { G.PROFILES[G.SETTINGS.profile].name } }
     end,
-	calculate = function(self, card, context)
+    calculate = function(self, card, context)
         if context.joker_main and #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit then
-            SMODS.add_card{set = "Planet"}
+            SMODS.add_card { set = "Planet" }
         end
     end,
     set_ability = function(self, card, initial, delay_sprites)
         card:add_sticker("perishable", true)
     end,
-    ppu_artist = {"hatstack"},
-    ppu_coder = {"evgast"},
-    ppu_team = {"thorn"},
+    ppu_artist = { "hatstack" },
+    ppu_coder = { "evgast" },
+    ppu_team = { "thorn" },
 }
 
 SMODS.Joker {
-	key = 'thorn_binary',
-	rarity = 1,
+    key = 'thorn_binary',
+    rarity = 1,
     atlas = "thorn_cards",
-	pos = { x = 1, y = 0 },
-	cost = 5,
-	blueprint_compat = false,
-	eternal_compat = false,
-	perishable_compat = true,
+    pos = { x = 1, y = 0 },
+    cost = 5,
+    blueprint_compat = false,
+    eternal_compat = false,
+    perishable_compat = true,
     jand_gemini_compat = false,
-    attributes = {'space', 'planet', 'chance'},
+    attributes = { 'space', 'planet', 'chance' },
     config = { extra = { odds = 2, times = 2 } },
     loc_vars = function(self, info_queue, card)
         local numerator, denominator = SMODS.get_probability_vars(card, 1, card.ability.extra.odds, 'thorn_binary')
-		return { vars = { numerator, denominator} }
+        return { vars = { numerator, denominator } }
     end,
-    ppu_artist = {"willow"},
-    ppu_coder = {"evgast"},
-    ppu_team = {"thorn"},
+    ppu_artist = { "willow" },
+    ppu_coder = { "evgast" },
+    ppu_team = { "thorn" },
 }
 
 --Not sure if I should like make a file for this stuff but
-Wormhole.suits_dark = {Spades = true, Clubs = true}
-Wormhole.suits_light = {Hearts = true, Diamonds = true}
+Wormhole.suits_dark = { Spades = true, Clubs = true }
+Wormhole.suits_light = { Hearts = true, Diamonds = true }
 
 function suit_count(suit)
     local count = 0
@@ -78,35 +78,39 @@ function suit_count(suit)
 end
 
 SMODS.Joker {
-	key = 'thorn_derse',
-	rarity = 2,
+    key = 'thorn_derse',
+    rarity = 2,
     atlas = "thorn_cards",
-	pos = { x = 2, y = 0 },
-	cost = 7,
-	blueprint_compat = true,
-	eternal_compat = false,
-	perishable_compat = true,
+    pos = { x = 2, y = 0 },
+    cost = 7,
+    blueprint_compat = true,
+    eternal_compat = false,
+    perishable_compat = true,
     jand_gemini_compat = true,
-    attributes = {'space', 'xmult', 'spades', 'clubs', "suit", "scaling", "full_deck"},
+    attributes = { 'space', 'xmult', 'spades', 'clubs', "suit", "scaling", "full_deck" },
     config = { extra = { xmult_base = 1, xmult_gain = 0.1, xmult = 1, percentage_floor = 45 } },
     loc_vars = function(self, info_queue, card)
         if card.area.config.type == "Joker" then
-            local percentage = math.floor(((suit_count("dark")/#G.playing_cards)*100)+0.5)
+            local percentage = math.floor(((suit_count("dark") / #G.playing_cards) * 100) + 0.5)
             if percentage >= card.ability.extra.percentage_floor then
-                card.ability.extra.xmult = card.ability.extra.xmult_base + ((percentage - card.ability.extra.percentage_floor) * card.ability.extra.xmult_gain)
+                card.ability.extra.xmult = card.ability.extra.xmult_base +
+                ((percentage - card.ability.extra.percentage_floor) * card.ability.extra.xmult_gain)
             else
                 card.ability.extra.xmult = card.ability.extra.xmult_base
             end
-            return { vars = { card.ability.extra.xmult_gain, card.ability.extra.xmult, card.ability.extra.percentage_floor, percentage }, key = self.key }
+            return { vars = { card.ability.extra.xmult_gain, card.ability.extra.xmult, card.ability.extra.percentage_floor, percentage }, key =
+            self.key }
         else
-            return { vars = { card.ability.extra.xmult_gain, card.ability.extra.xmult, card.ability.extra.percentage_floor }, key = self.key .. "_collection" }
+            return { vars = { card.ability.extra.xmult_gain, card.ability.extra.xmult, card.ability.extra.percentage_floor }, key =
+            self.key .. "_collection" }
         end
     end,
     calculate = function(self, card, context)
         if context.joker_main then
-            local percentage = math.floor(((suit_count("dark")/#G.playing_cards)*100)+0.5)
+            local percentage = math.floor(((suit_count("dark") / #G.playing_cards) * 100) + 0.5)
             if percentage >= card.ability.extra.percentage_floor then
-                card.ability.extra.xmult = card.ability.extra.xmult_base + ((percentage - card.ability.extra.percentage_floor) * card.ability.extra.xmult_gain)
+                card.ability.extra.xmult = card.ability.extra.xmult_base +
+                ((percentage - card.ability.extra.percentage_floor) * card.ability.extra.xmult_gain)
             else
                 card.ability.extra.xmult = card.ability.extra.xmult_base
             end
@@ -115,56 +119,73 @@ SMODS.Joker {
             }
         end
     end,
-    ppu_artist = {"hatstack"},
-    ppu_coder = {"evgast"},
-    ppu_team = {"thorn"},
+    ppu_artist = { "hatstack" },
+    ppu_coder = { "evgast" },
+    ppu_team = { "thorn" },
 }
 
 SMODS.Joker { --Moved here for the sake of them being close on atlas
-	key = 'thorn_prospit',
-	rarity = 2,
+    key = 'thorn_prospit',
+    rarity = 2,
     atlas = "thorn_cards",
-	pos = { x = 1, y = 1 },
-	cost = 7,
-	blueprint_compat = true,
-	eternal_compat = true,
-	perishable_compat = false,
+    pos = { x = 1, y = 1 },
+    cost = 7,
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = false,
     jand_gemini_compat = false,
     config = { extra = { chips = 0 } },
-    attributes = {"scaling", "chips", "suit", "hearts", "diamonds", "rank", "passive", "space"},
+    attributes = { "scaling", "chips", "suit", "hearts", "diamonds", "rank", "passive", "space" },
     loc_vars = function(self, info_queue, card)
         return { vars = { card.ability.extra.chips } }
     end,
     calculate = function(self, card, context)
-        if context.joker_main then
-            local percentage = ((suit_count("dark")/#G.playing_cards)*100)
-            if percentage >= card.ability.extra.percentage_floor then
-                card.ability.extra.xmult = card.ability.extra.xmult_base + ((percentage - card.ability.extra.percentage_floor) * card.ability.extra.xmult_gain)
-            else
-                card.ability.extra.xmult = card.ability.extra.xmult_base
+        if context.individual and context.cardarea == G.play then
+            for suit, _ in pairs(Wormhole.suits_light) do
+                if context.other_card:is_suit(suit) then
+                    SMODS.scale_card(card, {
+                        ref_table = card.ability.extra,
+                        ref_value = "chips",
+                        scalar_table = context.other_card.base,
+                        scalar_value = "nominal",
+                        no_message = true,
+                    })
+                    return {
+                        message = localize('k_upgrade_ex'),
+                        colour = G.C.ATTENTION,
+                        message_card = card
+                    }
+                end
             end
+        elseif context.stay_flipped and context.to_area == G.hand and not context.blueprint then
+            for suit, _ in pairs(Wormhole.suits_light) do
+                if context.other_card:is_suit(suit) then
+                    return { stay_flipped = true }
+                end
+            end
+        elseif context.cardarea == G.jokers and context.joker_main and context.scoring_hand and card.ability.extra.chips > 0 then
             return {
-                xmult = card.ability.extra.xmult
+                chips = card.ability.extra.chips,
             }
         end
     end,
-    ppu_artist = {"hatstack"},
-    ppu_coder = {"sophie"},
-    ppu_team = {"thorn"},
+    ppu_artist = { "hatstack" },
+    ppu_coder = { "sophie" },
+    ppu_team = { "thorn" },
 }
 
 SMODS.Joker {
-	key = 'thorn_mars_face',
-	rarity = 2,
+    key = 'thorn_mars_face',
+    rarity = 2,
     atlas = "thorn_cards",
-	pos = { x = 3, y = 0 },
-	cost = 6,
-	blueprint_compat = true,
-	eternal_compat = false,
-	perishable_compat = true,
+    pos = { x = 3, y = 0 },
+    cost = 6,
+    blueprint_compat = true,
+    eternal_compat = false,
+    perishable_compat = true,
     jand_gemini_compat = false,
-    attributes = {'space', 'hand_type', 'generation', "face"},
-    config = { extra = {  } },
+    attributes = { 'space', 'hand_type', 'generation', "face" },
+    config = { extra = {} },
     loc_vars = function(self, info_queue, card)
     end,
     calculate = function(self, card, context) --Most of this is vremade code for DNA :Þ
@@ -200,41 +221,48 @@ SMODS.Joker {
         end
     end,
     set_sprites = function(self, card, front)
-        if math.random() > 1/2 then
-            card.children.center:set_sprite_pos({x = 4, y = 0})
+        if math.random() > 1 / 2 then
+            card.children.center:set_sprite_pos({ x = 4, y = 0 })
         end
     end,
-    ppu_artist = {"hatstack"},
-    ppu_coder = {"evgast"},
-    ppu_team = {"thorn"},
+    ppu_artist = { "hatstack" },
+    ppu_coder = { "evgast" },
+    ppu_team = { "thorn" },
 }
 
 SMODS.Joker {
-	key = 'thorn_piss',
-	rarity = 2,
+    key = 'thorn_piss',
+    rarity = 2,
     atlas = "thorn_cards",
-	pos = { x = 0, y = 1 },
-	cost = 6,
-	blueprint_compat = true,
-	eternal_compat = false,
-	perishable_compat = true,
+    pos = { x = 0, y = 1 },
+    cost = 6,
+    blueprint_compat = true,
+    eternal_compat = false,
+    perishable_compat = true,
     jand_gemini_compat = false,
-    attributes = {"space", "clubs", "suit", "modify_card", "perma_bonus"}, --I'm not sure of this should have 'diamonds' attribute because of the way SMODS wiki describes these -- BenRoffey: correct to omit
+    attributes = { "space", "clubs", "suit", "modify_card", "perma_bonus" }, --I'm not sure of this should have 'diamonds' attribute because of the way SMODS wiki describes these -- BenRoffey: correct to omit
     config = { extra = { suit_old = "Clubs", suit_new = "Diamonds", chips = 20 } },
     loc_vars = function(self, info_queue, card)
         local loc = {}
-	    local quip = {}
-	    localize{type = "descriptions", key = "thorn_piss_rant", set = "Joker", default_col = G.C.UI.TEXT_DARK, nodes = loc, vars = {colours = {G.C.RED}}, scale = 0.7}
-	    quip = {transparent_multiline_text(loc)}
+        local quip = {}
+        localize { type = "descriptions", key = "thorn_piss_rant", set = "Joker", default_col = G.C.UI.TEXT_DARK, nodes = loc, vars = { colours = { G.C.RED } }, scale = 0.7 }
+        quip = { transparent_multiline_text(loc) }
         local rant
         if not Wormhole.config.family_friendly then
-            rant = {n=G.UIT.C, config = {align = "cm", minh = 1, minw = 5, r = 0.3, padding = 0.07, colour = G.C.JOKER_GREY, shadow = true}, nodes={
-                {n=G.UIT.R, config={align = "cm" , r = 0.2, padding = 0.1, colour = G.C.WHITE}, nodes=
-                    quip
+            rant = {
+                n = G.UIT.C,
+                config = { align = "cm", minh = 1, minw = 5, r = 0.3, padding = 0.07, colour = G.C.JOKER_GREY, shadow = true },
+                nodes = {
+                    {
+                        n = G.UIT.R,
+                        config = { align = "cm", r = 0.2, padding = 0.1, colour = G.C.WHITE },
+                        nodes =
+                            quip
+                    }
                 }
-            }}
+            }
         end
-        return { key = self.key .. (Wormhole.config.family_friendly and "_alt" or ""), vars = { localize(card.ability.extra.suit_old, 'suits_plural'), localize(card.ability.extra.suit_new, 'suits_plural'), card.ability.extra.chips }, main_end = {rant} }
+        return { key = self.key .. (Wormhole.config.family_friendly and "_alt" or ""), vars = { localize(card.ability.extra.suit_old, 'suits_plural'), localize(card.ability.extra.suit_new, 'suits_plural'), card.ability.extra.chips }, main_end = { rant } }
     end,
     calculate = function(self, card, context)
         if context.individual and context.cardarea == G.play then
@@ -255,34 +283,35 @@ SMODS.Joker {
             }
         end
     end,
-    ppu_artist = {"hatstack"},
-    ppu_coder = {"evgast"},
-    ppu_team = {"thorn"},
+    ppu_artist = { "hatstack" },
+    ppu_coder = { "evgast" },
+    ppu_team = { "thorn" },
 }
 
 SMODS.Joker {
-	key = 'thorn_devils_machine',
-	rarity = 3,
+    key = 'thorn_devils_machine',
+    rarity = 3,
     atlas = "thorn_cards",
-	pos = { x = 3, y = 1 },
-	cost = 7,
-	blueprint_compat = true,
-	eternal_compat = false,
-	perishable_compat = true,
+    pos = { x = 3, y = 1 },
+    cost = 7,
+    blueprint_compat = true,
+    eternal_compat = false,
+    perishable_compat = true,
     jand_gemini_compat = true,
     config = { extra = { stop = false, odds = 4 } },
-    attributes = {"retrigger", "chance", "space"},
+    attributes = { "retrigger", "chance", "space" },
     loc_vars = function(self, info_queue, card)
-        local new_numerator, new_denominator = SMODS.get_probability_vars(card, 1, card.ability.extra.odds, 'devils_machine')
-        return {vars = {new_numerator, new_denominator}}
+        local new_numerator, new_denominator = SMODS.get_probability_vars(card, 1, card.ability.extra.odds,
+            'devils_machine')
+        return { vars = { new_numerator, new_denominator } }
     end,
     calculate = function(self, card, context, effect)
         if (not card.ability.extra.stop) and context.individual and context.cardarea == G.play then
             if context.other_card == context.scoring_hand[#context.scoring_hand] then
                 return {
                     func = function()
-                        SMODS.calculate_effect({message = localize('k_again_ex')}, card)
-                        for i=#context.scoring_hand, 1, -1 do
+                        SMODS.calculate_effect({ message = localize('k_again_ex') }, card)
+                        for i = #context.scoring_hand, 1, -1 do
                             local passed_context = context
                             card.ability.extra.stop = true
                             SMODS.score_card(context.scoring_hand[i], passed_context)
@@ -305,43 +334,43 @@ SMODS.Joker {
             end
         end
     end,
-    ppu_artist = {"hatstack"},
-    ppu_coder = {"sophie"},
-    ppu_team = {"thorn"},
+    ppu_artist = { "hatstack" },
+    ppu_coder = { "sophie" },
+    ppu_team = { "thorn" },
 }
 
 SMODS.Joker {
-	key = 'thorn_giygas',
-	rarity = 3,
+    key = 'thorn_giygas',
+    rarity = 3,
     atlas = "thorn_cards",
-	pos = { x = 4, y = 1 },
-	cost = 7,
-	blueprint_compat = true,
-	eternal_compat = false,
-	perishable_compat = true,
+    pos = { x = 4, y = 1 },
+    cost = 7,
+    blueprint_compat = true,
+    eternal_compat = false,
+    perishable_compat = true,
     jand_gemini_compat = true,
     no_collection = true,
     in_pool = function(self)
         return false
     end,
-    attributes = {"retrigger", "chance", "modify_card", "enhancements", "space"},
+    attributes = { "retrigger", "chance", "modify_card", "enhancements", "space" },
     config = { extra = { stop = false, odds = 4 } },
     loc_vars = function(self, info_queue, card)
         local new_numerator, new_denominator = SMODS.get_probability_vars(card, 1, card.ability.extra.odds, 'giygas')
-        return {vars = {new_numerator, new_denominator}}
+        return { vars = { new_numerator, new_denominator } }
     end,
     calculate = function(self, card, context, effect)
         if (not card.ability.extra.stop) and context.individual and context.cardarea == G.play then
             if context.other_card == context.scoring_hand[#context.scoring_hand] then
                 return {
                     func = function()
-                        SMODS.calculate_effect({message = localize('k_again_ex')}, card)
+                        SMODS.calculate_effect({ message = localize('k_again_ex') }, card)
                         local scoring_hand_giygas = {}
                         for i, v in ipairs(context.scoring_hand) do
                             scoring_hand_giygas[i] = v
                         end
                         pseudoshuffle(scoring_hand_giygas, 'giygas')
-                        for i=1, #scoring_hand_giygas do
+                        for i = 1, #scoring_hand_giygas do
                             local passed_context = context
                             card.ability.extra.stop = true
                             SMODS.score_card(scoring_hand_giygas[i], passed_context)
@@ -355,7 +384,7 @@ SMODS.Joker {
             for _, scored_card in ipairs(context.scoring_hand) do
                 if SMODS.pseudorandom_probability(card, 'giygas', 1, card.ability.extra.odds, 'giygas') then
                     triggered = true
-                    scored_card:set_ability(SMODS.poll_enhancement{key = "giygas", guaranteed = true}, nil, true)
+                    scored_card:set_ability(SMODS.poll_enhancement { key = "giygas", guaranteed = true }, nil, true)
                     G.E_MANAGER:add_event(Event({
                         func = function()
                             scored_card:juice_up()
@@ -371,7 +400,7 @@ SMODS.Joker {
             end
         end
     end,
-    ppu_artist = {"hatstack"},
-    ppu_coder = {"sophie"},
-    ppu_team = {"thorn"},
+    ppu_artist = { "hatstack" },
+    ppu_coder = { "sophie" },
+    ppu_team = { "thorn" },
 }
