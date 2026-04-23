66ca8eb70d3b6b043559b5895a50145dfd080725
the-Astra <djs8743@rit.edu>
2026-04-22 21:50:14 -0400

    Fix Star Map incorrect render position (Thank you TLR)

--- content/TheLastResort/3_items/star_map.lua
+++ content/TheLastResort/3_items/star_map.lua
@@ -15,46 +15,9 @@ SMODS.Consumable{
 		return {vars = {colours = {SMODS.ConsumableTypes.worm_tlr_constellation.secondary_colour}}}
 	end,
 	set_sprites = function (self, card, front)
-		card.worm_tlr_canvas_text = SMODS.CanvasSprite{
-			worm_tlr_func = function(self, other_obj, ms, mr, mx, my)
-				love.graphics.push()
-				local scale = (other_obj.scale_mag or other_obj.VT.scale)
-				love.graphics.scale(scale)
-				love.graphics.translate(other_obj.T.w/2, other_obj.T.h/2)
-				love.graphics.scale(1/scale)
-				love.graphics.stencil(function()
-					love.graphics.circle("fill", 0, 0, 25)
-				end, "replace", 1)
-				love.graphics.setStencilTest("greater", 0)
-				love.graphics.push()
-				love.graphics.origin()
-				local shader = love.graphics.getShader()
-				love.graphics.setShader()
-				local x, y = love.mouse.getPosition()
-				local img = SMODS.Atlases.worm_tlr_starmap.image
-				--love.graphics.scale(scale)
-				--love.graphics.translate(other_obj.T.x, other_obj.T.y)
-				love.graphics.translate(img:getWidth() / 2, img:getHeight() / 2)
-    		prep_draw(other_obj, (1 + (ms or 0)), (mr or 0), self.ARGS.draw_from_offset, true)
-				love.graphics.scale(1/scale)
-				love.graphics.translate((x) / 10, (y) / 10)
-				love.graphics.translate(-img:getWidth() / 2, -img:getHeight() / 2)
-				love.graphics.draw(img, 0, 0, 0, 0.8, 0.8) -- arbitrary scaling values
-				love.graphics.setShader(shader)
-				love.graphics.pop()
-				love.graphics.pop()
-				love.graphics.setStencilTest()
-				love.graphics.pop()
-			end
-		}
+		card.worm_tlr_canvas_text = SMODS.CanvasSprite{}
 	end,
 	use = function (self, card, area, copier)
-		local constellations = {}
-		for _, _card in pairs(G.consumeables.cards) do
-			if _card.ability.set == 'worm_tlr_constellation' then
-				constellations[#constellations+1] = _card
-			end
-		end
 		G.E_MANAGER:add_event(Event({
 			trigger = 'after',
 			delay = 0.4,
@@ -64,20 +27,20 @@ SMODS.Consumable{
 				return true
 			end
 		}))
-		for i, _card in ipairs(constellations) do
+		for _, _card in ipairs(G.consumeables.cards) do
 			G.E_MANAGER:add_event(Event({
 				trigger = 'after',
 				delay = 0.15,
 				func = function()
 					_card:flip()
-					play_sound('card1', 1.15 - (i - 0.999) / (#constellations - 0.998) * 0.3)
+					play_sound('card1')
 					_card:juice_up(0.3, 0.3)
 					return true
 				end
 			}))
 		end
 		delay(0.2)
-		for _, _card in ipairs(constellations) do
+		for _, _card in ipairs(G.consumeables.cards) do
 			G.E_MANAGER:add_event(Event({
 				trigger = 'after',
 				delay = 0.1,
@@ -88,13 +51,13 @@ SMODS.Consumable{
 				end
 			}))
 		end
-		for i, _card in ipairs(constellations) do
+		for _, _card in ipairs(G.consumeables.cards) do
 			G.E_MANAGER:add_event(Event({
 				trigger = 'after',
 				delay = 0.15,
 				func = function()
 					_card:flip()
-					play_sound('tarot2', 0.85 + (i-0.999)/(#constellations-0.998)*0.3)
+					play_sound('tarot2', 1, 0.6)
 					_card:juice_up(0.3, 0.3)
 					return true
 				end
@@ -125,8 +88,53 @@ SMODS.DrawStep {
 				love.graphics.push()
 				love.graphics.origin()
 				sprite.canvas:renderTo(love.graphics.clear, 0, 0, 0, 0)
-				local text = love.graphics.newText(sprite.font, {sprite.text_colour or G.C.UI.TEXT_LIGHT, sprite.ref_table and sprite.ref_table[sprite.ref_value] or sprite.text})
-				local scale_fac = math.min((sprite.text_width or sprite.canvasW)/text:getWidth(), (sprite.text_height or sprite.canvasH)/text:getHeight()) * sprite.canvasScale
+				local canv = love.graphics.getCanvas()
+				local other_obj = self.children.center
+				love.graphics.setCanvas({sprite.canvas, stencil=true})
+					love.graphics.push()
+					local scale = (other_obj.scale_mag or other_obj.VT.scale)
+					--[[
+					love.graphics.scale(scale)
+					love.graphics.translate(other_obj.T.w/2, other_obj.T.h/2)
+					love.graphics.scale(1/scale)
+					love.graphics.stencil(function()
+						love.graphics.circle("fill", 0, 0, 25)
+					end, "replace", 1)
+					love.graphics.setStencilTest("greater", 0)
+					]]
+					love.graphics.push()
+					love.graphics.origin()
+					local shader = love.graphics.getShader()
+					love.graphics.setShader()
+					local x, y = love.mouse.getPosition()
+					local width, height = love.graphics.getDimensions()
+					local img = SMODS.Atlases.worm_tlr_starmap.image
+					local arbitrary_scaling = 10
+					local arbitrary_scaling_part_2 = 2.5
+					local padding = 0.9
+					--love.graphics.scale(scale)
+					--love.graphics.translate(other_obj.T.x, other_obj.T.y)
+					--love.graphics.translate(img:getWidth() / 2, img:getHeight() / 2)
+   	 			--prep_draw(other_obj, 1, 0, self.ARGS.draw_from_offset, true)
+					--prep_draw(other_obj, 1)
+					--love.graphics.scale(1/(other_obj.scale.x/other_obj.VT.w), 1/(other_obj.scale.y/other_obj.VT.h))
+					--love.graphics.scale(1/scale)
+					local tilescale = G.TILESIZE
+					love.graphics.translate(other_obj.VT.w * scale / 4 * tilescale, other_obj.VT.h * scale / 4 * tilescale)
+					love.graphics.stencil(function()
+						love.graphics.circle("fill", 0, 0, 25 * G.TILESCALE * arbitrary_scaling_part_2)
+					end, "replace", 1)
+					love.graphics.setStencilTest("greater", 0)
+					love.graphics.scale(arbitrary_scaling)
+					love.graphics.translate((x - (width / 2)) * (img:getWidth() / width) * padding, (y - (height / 2)) * (img:getHeight() / height) * padding)
+					love.graphics.translate(-img:getWidth() / 2, -img:getHeight() / 2)
+					love.graphics.draw(img, 0, 0, 0, 1, 1)
+					love.graphics.setShader(shader)
+					--love.graphics.pop()
+					love.graphics.pop()
+					love.graphics.setStencilTest()
+					love.graphics.pop()
+				love.graphics.setCanvas({canv, stencil=true})
 				love.graphics.pop()
 				sprite.role.draw_major = self
 				sprite:draw_shader('dissolve', nil, nil, nil, self.children.center)
